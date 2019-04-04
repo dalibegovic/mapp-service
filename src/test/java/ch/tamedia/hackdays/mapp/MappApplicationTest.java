07b37ddbe638f64566f2ca428dc0facfc9e9cbb7
Damir Alibegovic <damir.alibegovic@olmero.ch>
2019-04-04 18:04:29 +0200

    Added tests back again

---
+++ src/test/java/ch/tamedia/hackdays/mapp/MappApplicationTest.java
@@ -0,0 +1,12 @@
+package ch.tamedia.hackdays.mapp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MappApplicationTest {
+	@Test
+	public void contextLoads() {
+
+	}
+}
